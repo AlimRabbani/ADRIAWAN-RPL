5866ea7166c3439fdcf8bb1ac1b5925c22bffb8d
AlimRabbani <alimmanurung18@gmail.com>
2022-10-19 21:03:37 +0800

    rpl day 3

---
+++ data.py
@@ -0,0 +1,11 @@
+nama = "adriawan"
+asal = "mamasa"
+hobby = "volly"
+status = "mahasiswa"
+umur = 21
+
+print ("nama saya :",nama)
+print("saya berasal dari :",asal)
+print("hobby saya :",hobby)
+print("status saya :",status)
+print("umur saya :",umur)
